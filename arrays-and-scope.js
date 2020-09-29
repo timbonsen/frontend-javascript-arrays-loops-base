2fea3ded011d2aad0e2c0eda3ce4a0b9f808996d
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-09-29 12:25:28 +0200

    create exercises

---
+++ arrays-and-scope.js
@@ -0,0 +1,22 @@
+
+const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+
+// --------------------------
+// Opdracht 1: Check hoeveel studenten hun studie cum laude hebben gehaald (8 of hoger) en log dit in de console.
+// --------------------------
+
+
+// --------------------------
+// Opdracht 2: Check welke waarde in de array het hoogste is, en log dit in de console
+// --------------------------
+
+
+// --------------------------
+// Opdracht 3: Tel alle waardes van de array bij elkaar op en bereken het gemiddelde. Log dit in de console.
+// --------------------------
+
+
+
+
+
+
---
+++ arrays.js
@@ -0,0 +1,23 @@
+// --------------------------
+// Opdracht 1
+// --------------------------
+
+// Zorg ervoor dat er -"je" achter iedere naam wordt geplakt
+// geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+
+
+
+// --------------------------
+// Opdracht 2
+// --------------------------
+// Vermenigvuldig iedere waarde in de array met 3
+// geeft: [ 6, 12, 15, 87, 114 ];
+
+
+
+// --------------------------
+// Opdracht 3
+// --------------------------
+// Tel 3 op bij ieder item in de array
+// geeft: [ 7, 9, 15, 37, 4 ]
+
