36ebbd22d376605971bfbd1ff355765fcd84aef8
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-09-29 16:41:09 +0200

    Made questions clearer

--- arrays-and-scope.js
+++ arrays-and-scope.js
@@ -3,16 +3,19 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // --------------------------
 // Opdracht 1: Check hoeveel studenten hun studie cum laude hebben gehaald (8 of hoger) en log dit in de console.
+// verwacht antwoord: 6
 // --------------------------
 
 
 // --------------------------
 // Opdracht 2: Check welke waarde in de array het hoogste is, en log dit in de console
+// verwacht antwoord: 9
 // --------------------------
 
 
 // --------------------------
 // Opdracht 3: Tel alle waardes van de array bij elkaar op en bereken het gemiddelde. Log dit in de console.
+// verwacht antwoord: 6.642857142857143
 // --------------------------
 
 
--- arrays.js
+++ arrays.js
@@ -4,8 +4,9 @@
 
 let names = ["Henk", "Piet", "Fred", "Joop"];
 
-// Zorg ervoor dat er -"je" achter iedere naam wordt geplakt
-// geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+// Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
+// Wanneer je de volledige array logt zie je alle nieuwe waardes
+// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
 
 
@@ -15,8 +16,9 @@ let names = ["Henk", "Piet", "Fred", "Joop"];
 
 const numbers = [2, 4, 5, 29, 38];
 
-// Vermenigvuldig iedere waarde in de array met 3
-// geeft: [ 6, 12, 15, 87, 114 ];
+// Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
+// Wanneer je de volledige array logt zie je alle nieuwe waardes
+// console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
 
 
 
@@ -26,7 +28,8 @@ const numbers = [2, 4, 5, 29, 38];
 
 const figures = [4, 6, 12, 34, 1];
 
-// Tel 3 op bij ieder item in de array
-// geeft: [ 7, 9, 15, 37, 4 ]
+// Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
+// Wanneer je de volledige array logt zie je alle nieuwe waardes
+// console.log(figures) geeft: [ 7, 9, 15, 37, 4 ]
 
 
