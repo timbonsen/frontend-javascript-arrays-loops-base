6b32f266b2bbbd9534649b09dcbc7aa961a26656
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-09-29 12:31:56 +0200

    added example arrays

--- arrays.js
+++ arrays.js
@@ -2,6 +2,8 @@
 // Opdracht 1
 // --------------------------
 
+let names = ["Henk", "Piet", "Fred", "Joop"];
+
 // Zorg ervoor dat er -"je" achter iedere naam wordt geplakt
 // geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
@@ -10,6 +12,9 @@
 // --------------------------
 // Opdracht 2
 // --------------------------
+
+const numbers = [2, 4, 5, 29, 38];
+
 // Vermenigvuldig iedere waarde in de array met 3
 // geeft: [ 6, 12, 15, 87, 114 ];
 
@@ -18,6 +23,10 @@
 // --------------------------
 // Opdracht 3
 // --------------------------
+
+const figures = [4, 6, 12, 34, 1];
+
 // Tel 3 op bij ieder item in de array
 // geeft: [ 7, 9, 15, 37, 4 ]
+
 
