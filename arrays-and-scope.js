bd890bbf83f717019f9126e20f00fd3d49b575ae
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-10-13 16:17:11 +0200

    Add answers

--- arrays-and-scope.js
+++ arrays-and-scope.js
@@ -1,4 +1,3 @@
-
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 // --------------------------
@@ -6,11 +5,39 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // verwacht antwoord: 6
 // --------------------------
 
+// maak een variabele die buiten de scope van de for-loop (anders kan de console.log er niet bij)
+// dit is de "teller" die we telkens willen verhogen als de for-loop een 8 of hoger tegenkomt
+let cumLaude = 0;
+
+for (let i = 0; i < grades.length; i++) {
+  if (grades[i] >= 8) {
+    cumLaude = cumLaude + 1;
+    // dit mag ook: cumLaude += 1;
+    // dit mag ook: cumLaude++;
+  }
+}
+
+console.log(cumLaude);
 
 // --------------------------
 // Opdracht 2: Check welke waarde in de array het hoogste is, en log dit in de console
 // verwacht antwoord: 9
 // --------------------------
+
+// maak een variabele die buiten de scope van de for-loop (anders kan de console.log er niet bij)
+// dit is de "container" waarin we telkens de hoogste waarde in opslaan, tot we een hogere waarde tegenkomen
+
+let highest = 0;
+
+for (let i = 0; i < grades.length; i++) {
+  // als de huidige waarde van grades (grades[i]) hoger is dan mijn huidige opgeslagen waarde
+  // sla dan dat nieuwe cijfer op als hoogste waarde
+  if (grades[i] > highest) {
+    highest = grades[i];
+  }
+}
+
+console.log(highest);
 
 
 // --------------------------
@@ -18,7 +45,26 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // verwacht antwoord: 6.642857142857143
 // --------------------------
 
+// maak een variabele die buiten de scope van de for-loop (anders kan de console.log er niet bij)
+// dit is de "container" waarbij we elke waarde in de array willen optellen.
+let average = 0;
 
+for (let i = 0; i < grades.length; i++) {
+  // tel telkens de waarde van de entry uit de array bij wat we daarvoor al hebben opgetelt
+  average = average + grades[i];
+}
+
+console.log(average / grades.length);
+// dit mag ook:
+// const calculatedAverage = average / grades.length;
+// console.log(calculatedAverage);
+
+// UITLEG
+// eerste iteratie met i = 0 ---> average = 0 (de huidige waarde van average) + 9;
+// tweede iteratie met i = 1 ---> average = 9 (de huidige waarde van average) + 8;
+// derde iteratie met i = 2 ---> average = 17 (de huidige waarde van average) + 5;
+// vierde iteratie met i = 3 ---> average = 22 (de huidige waarde van average) + 7;
+// etc.
 
 
 
--- arrays.js
+++ arrays.js
@@ -1,24 +1,50 @@
 // --------------------------
 // Opdracht 1
 // --------------------------
+// Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
+// Wanneer je de volledige array logt, zie je alle nieuwe waardes
+// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
 let names = ["Henk", "Piet", "Fred", "Joop"];
 
-// Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
-// Wanneer je de volledige array logt zie je alle nieuwe waardes
-// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+for (let i = 0; i < names.length; i++) {
+  names[i] = names[i] + 'je';
+}
+
+console.log(names);
+
+// UITLEG
+// eerste iteratie met i = 0 ---> names[0] = names[0] + "je";
+// tweede iteratie met i = 1 ---> names[1] = names[1] + "je";
+// derde iteratie met i = 2 ---> names[2] = names[2] + "je";
+// vierde iteratie met i = 3 ---> names[3] = names[3] + "je";
 
 
 
 // --------------------------
 // Opdracht 2
 // --------------------------
-
-const numbers = [2, 4, 5, 29, 38];
-
 // Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
+
+const numbers = [2, 4, 5, 29, 38];
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] = numbers[i] * 3;
+
+  // als je alleen de uitkomst logt (zoals hieronder) zou je de array niet veranderen:
+  // console.log(numbers[i] * 3);
+}
+
+console.log(numbers);
+
+// UITLEG
+// eerste iteratie met i = 0 ---> numbers[0] = numbers[0] * 3;
+// tweede iteratie met i = 1 ---> numbers[1] = numbers[1] * 3;
+// derde iteratie met i = 2 ---> numbers[2] = numbers[2] * 3;
+// vierde iteratie met i = 3 ---> numbers[3] = numbers[3] * 3;
+// vijfde iteratie met i = 4 ---> numbers[4] = numbers[4] * 3;
 
 
 
@@ -26,10 +52,21 @@ const numbers = [2, 4, 5, 29, 38];
 // Opdracht 3
 // --------------------------
 
-const figures = [4, 6, 12, 34, 1];
-
 // Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(figures) geeft: [ 7, 9, 15, 37, 4 ]
 
+const figures = [4, 6, 12, 34, 1];
 
+for (let i = 0; i < figures.length; i++) {
+  figures[i] = figures[i] + 3;
+}
+
+console.log(figures);
+
+// UITLEG
+// eerste iteratie met i = 0 ---> figures[0] = figures[0] + 3;
+// tweede iteratie met i = 1 ---> figures[1] = figures[1] + 3;
+// derde iteratie met i = 2 ---> figures[2] = figures[2] + 3;
+// vierde iteratie met i = 3 ---> figures[3] = figures[3] + 3;
+// vijfde iteratie met i = 4 ---> figures[4] = figures[4] + 3;
